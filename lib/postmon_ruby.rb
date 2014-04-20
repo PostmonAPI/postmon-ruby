a645f5c65c7e63e3edfc0ac67fddeb6d7d048352
Carlos Ribeiro <mail@carlosribeiro.me>
2014-04-20 16:37:23 +0000

    removed pry

--- lib/postmon_ruby.rb
+++ lib/postmon_ruby.rb
@@ -2,7 +2,6 @@ require "postmon_ruby/version"
 require "postmon_ruby/client"
 require "postmon_ruby/address"
 require "httparty"
-require "pry"
 
 module PostmonRuby
   ENDPOINT = "http://api.postmon.com.br"
