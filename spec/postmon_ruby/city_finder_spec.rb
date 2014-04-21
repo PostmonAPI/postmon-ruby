04e635d116cb0febf32cdf13e4a2a358d0cf1350
Carlos Ribeiro <mail@carlosribeiro.me>
2014-04-21 00:01:07 +0000

    adicionado teste para cidades com caracteres especiais

--- spec/postmon_ruby/city_finder_spec.rb
+++ spec/postmon_ruby/city_finder_spec.rb
@@ -27,6 +27,17 @@ module PostmonRuby
           end          
         end
 
+        context "with valid city with special characters" do
+          let(:city) { PostmonRuby::Client.search(:cidade, "SP", "São Paulo") }
+          it "returns a PostmonRuby::City object" do
+            expect(city).to be_a(PostmonRuby::City)
+          end
+          it "returns a complete city info" do
+            expect(city.area_km2).to eq "1521,101"
+            expect(city.codigo_ibge).to eq "3550308"
+          end          
+        end        
+
         context "with invalid city" do
           let(:city) { PostmonRuby::Client.search(:cidade, "SP", "Xxxx") }
           it "returns a PostmonRuby::City object" do
