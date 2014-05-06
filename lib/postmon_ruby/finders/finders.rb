443a785e1fcbca6119750d73cb6010ec93309d00
Carlos Ribeiro <mail@carlosribeiro.me>
2014-05-06 14:47:52 +0000

    alterado o formato de Hash para o padrao antigo

--- lib/postmon_ruby/finders/finders.rb
+++ lib/postmon_ruby/finders/finders.rb
@@ -2,10 +2,10 @@ module PostmonRuby
   module Finders
     class Finders
       @finders = { 
-        cep:    PostmonRuby::Finders::CepFinder.new,
-        cidade: PostmonRuby::Finders::CityFinder.new,
-        estado: PostmonRuby::Finders::StateFinder.new,
-        rastreio: PostmonRuby::Finders::TrackerFinder.new
+        :cep =>    PostmonRuby::Finders::CepFinder.new,
+        :cidade => PostmonRuby::Finders::CityFinder.new,
+        :estado => PostmonRuby::Finders::StateFinder.new,
+        :rastreio => PostmonRuby::Finders::TrackerFinder.new
       }
 
       def self.get_finder(finder)
